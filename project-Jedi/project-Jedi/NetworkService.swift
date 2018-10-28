0c5516d8bcfde942ff9324e186671c878d0f1f38
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-28 12:12:08 -0400

    Created NetworkService Swift file

---
+++ project-Jedi/project-Jedi/NetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  project-Jedi
+//
+//  Created by Kendall Poindexter on 10/28/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
