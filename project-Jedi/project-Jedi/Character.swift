85a0f21b7d7f4116152ff10db4507b508f7a73b0
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-28 12:02:04 -0400

    Created character struct with corresponding coding keys enum

---
+++ project-Jedi/project-Jedi/Character.swift
@@ -0,0 +1,25 @@
+//
+//  Character.swift
+//  project-Jedi
+//
+//  Created by Kendall Poindexter on 10/28/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct Characters {
+    let name: String
+    let birthYear: String
+    let gender: String
+    let homeworldURL: String
+    let speciesURL: [String]
+}
+
+enum CodingKeys: String, CodingKey {
+    case name
+    case birthYear = "birth_year"
+    case gender
+    case homeworldURL = "homeworld"
+    case speciesURL = "species"
+}
