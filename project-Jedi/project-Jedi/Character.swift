e68db6f324ed4cc17825125ac4072997262ff2ec
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-10-29 15:27:46 -0400

    Added variable mass to person class and assigned it to empty string

--- project-Jedi/project-Jedi/Character.swift
+++ project-Jedi/project-Jedi/Character.swift
@@ -14,6 +14,7 @@ class Person: Decodable {
     var gender: String = ""
     var homeworldURL: String = ""
     var speciesURL: [String] = []
+    var mass: String = ""
    
     enum CodingKeys: String, CodingKey {
         case name
